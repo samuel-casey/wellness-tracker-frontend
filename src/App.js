75fc1fdca68012f6526ffe277a04cf3ffe27c133
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-21 10:45:46 -0400

    basic bulma form jsx (still need to add name attrs)

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import './App.css';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import { Button } from 'react-bulma-components';
 import { Route, Link, Switch } from 'react-router-dom';
-import { Display } from './Components/Display';
+import { Display } from './Components/Display/Display';
+import { Form } from './Components/Form';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>What did you do to stay well today?</h1>
 			<Link to='/create'>
-				<Button color='info is-light'>Add an activity</Button>
+				<Button color='info is-light'>Add a new activity</Button>
 			</Link>
 			<Switch>
 				<Route exact path='/' render={(rp) => <Display {...rp} />} />
-				<Route path='/create' />
-				<Route path='/delete' />
+				<Route path='/create' render={(rp) => <Form {...rp} />} />
+				<Route path='/delete' render={(rp) => <Display {...rp} />} />
 			</Switch>
 		</div>
 	);
---
+++ src/Components/Form.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+export const Form = (props) => {
+	const [form, setForm] = useState({
+		day: '',
+		activity_type: '',
+		activity_mins: 0,
+		rating: 0,
+	});
+	const update = ({ target }) =>
+		setForm({ ...form, [target.name]: target.value });
+	return (
+		// onSubmit={handleSubmit}
+		<form>
+			<div className='field'>
+				<div className='control'>
+					<label className='label'>Day</label>
+					<input className='input' type='date' placeholder='' />
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<label className='label'>Activity</label>
+					<div className='select'>
+						<select style={{ width: '50vw' }}>
+							<option>Meditation</option>
+							<option>Exercise</option>
+							<option>Journaling</option>
+							<option>Connecting with friends/family</option>
+							<option>Other</option>
+						</select>
+					</div>
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<label className='label'>
+						How many minutes did you spend doing this activity?
+					</label>
+					<input className='input' type='number' placeholder='' />
+				</div>
+			</div>
+			<div className='field'>
+				<div className='control'>
+					<label className='label'>How did you feel afterwards?</label>
+					<label className='radio'>
+						<input type='radio' name='question' />1 :'(
+					</label>
+					<label className='radio'>
+						<input type='radio' name='question' />2 :(
+					</label>
+					<label className='radio'>
+						<input type='radio' name='question' />3 :|
+					</label>
+					<label className='radio'>
+						<input type='radio' name='question' />4 :)
+					</label>
+					<label className='radio'>
+						<input type='radio' name='question' />5 :D
+					</label>
+				</div>
+			</div>
+			<div className='buttons'>
+				<button className='is-info is-rounded is-fullwidth button' tabindex='0'>
+					Add
+				</button>
+			</div>
+		</form>
+	);
+};
