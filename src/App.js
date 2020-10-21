9113a95e3d4793427f69142b486f27c93b0b650b
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-20 23:04:22 -0400

    set up Display route and component file, added bulma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
+import { Route, Link, Switch } from 'react-router-dom';
+import { Display } from './Components/Display';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>What did you do to stay well today?</h1>
-			<button>Add an activity</button>
+			<Link to='/create'>
+				<Button color='info is-light'>Add an activity</Button>
+			</Link>
+			<Switch>
+				<Route exact path='/' render={(rp) => <Display {...rp} />} />
+				<Route path='/create' />
+				<Route path='/delete' />
+			</Switch>
 		</div>
 	);
 }
