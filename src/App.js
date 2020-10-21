8484680de6e1aa49087041d6cb8b47df8d08525f
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2020-10-20 22:37:58 -0400

    added CI= to react scripts build

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>What did you do to stay well today?</h1>
+		</div>
+	);
+}
+
+export default App;
